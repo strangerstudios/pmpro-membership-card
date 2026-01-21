c909d5a1bceae2712fa44fdaa24b5d684eb6373a
unknown <rachelrvasquez@gmail.com>
2026-01-21 13:11:25 -0500

    setting up dependencies and creating a new block

---
+++ blocks/build/pmpro-membership-card-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9ad7c079b99423f355b5');
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+/**
+ * `@wordpress/scripts` path-based name multi-block Webpack configuration.
+ * @see https://wordpress.stackexchange.com/questions/390282
+ */
+
+// Native Dependencies.
+const path = require("path");
+
+// Third-Party Dependencies.
+const CopyPlugin = require("copy-webpack-plugin");
+const config = require("@wordpress/scripts/config/webpack.config.js");
+
+config.entry = {
+  "pmpro-membership-card-block/index": path.resolve(
+    process.cwd(),
+    "blocks",
+    "src",
+    "pmpro-membership-card-block",
+    "index.js"
+  )
+};
+config.output = {
+  filename: "[name].js",
+  path: path.resolve(process.cwd(), "blocks", "build"),
+};
+
+// Add a CopyPlugin to copy over block.json files.
+config.plugins.push(
+  new CopyPlugin({
+    patterns: [
+      {
+        context: "blocks/src",
+        from: `*/block.json`,
+        noErrorOnMissing: true,
+      },
+      {
+        context: "blocks/src",
+        from: `*/render.php`,
+        noErrorOnMissing: true,
+      },
+    ],
+  })
+);
+
+module.exports = config;
