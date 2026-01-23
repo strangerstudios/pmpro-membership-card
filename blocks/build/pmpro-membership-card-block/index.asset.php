0ba973ec6b19f1ceab5390d77e79d70d95e1aa16
unknown <rachelrvasquez@gmail.com>
2026-01-22 22:38:26 -0500

    build doesn't seem to be up to date

--- blocks/build/pmpro-membership-card-block/index.asset.php
+++ blocks/build/pmpro-membership-card-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '99a0719e5ff9ae7f8ef6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '4e8a7fed1b19402c11ec');
