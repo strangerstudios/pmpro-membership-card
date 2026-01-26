9c2fdba5c130d35c47bd4021e7769abc2780e467
Andrew Lima <andrew@arctek.co.za>
2026-01-26 15:57:01 +0200

    re-run the build command

--- blocks/build/pmpro-membership-card-block/index.asset.php
+++ blocks/build/pmpro-membership-card-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5c129733e1a74344acd6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd124723f4d224788a521');
